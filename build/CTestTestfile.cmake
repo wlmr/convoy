d3027203154a680637a71e043c00e986fa7adb8c
ElMariachi92 <mister.trattner@gmail.com>
2019-10-08 15:18:37 +0200

    removes build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robproj/code/convoy/src
-# Build directory: /home/robproj/code/convoy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/robproj/code/convoy/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/robproj/code/wlmr/catkin_ws/devel;/home/robproj/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/robproj/code/wlmr/catkin_ws/devel;/home/robproj/catkin_ws/devel;/opt/ros/melodic" != "" else []
